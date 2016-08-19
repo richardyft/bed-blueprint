f831ce074861aa97a0353a3d257ea987359ba79f
Richard Tsai <rtsai@digitalriver.com>
2016-08-19 20:24:43 +0800

    Initial Commit

---
+++ src/main/java/agile/bdd_blueprint/App.java
@@ -0,0 +1,13 @@
+package agile.bdd_blueprint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
