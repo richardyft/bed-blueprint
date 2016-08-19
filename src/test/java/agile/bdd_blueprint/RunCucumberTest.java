d6c29aa2d6a99d8be34e86f01dd2244d7c4ea94b
Richard Tsai <rtsai@digitalriver.com>
2016-08-19 20:40:11 +0800

    Add RunCucumberTest.java

---
+++ src/test/java/agile/bdd_blueprint/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package agile.bdd_blueprint;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", plugin = { "pretty" })
+public class RunCucumberTest {
+}
